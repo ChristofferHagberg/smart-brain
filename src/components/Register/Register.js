338391df1c6f17deedc1c7cb2c38a17dacd7886c
Christoffer Hagberg <christoffer.hagberg@live.se>
2020-08-18 18:21:33 +0200

    fixed bug

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitChange = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://fast-shore-78645.herokuapp.com/register', {
             method: 'post',
             headers: {'content-type': 'application/json'},
             body: JSON.stringify({
